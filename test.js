93b060231b826884eb003b15aff80aaf00c19f01
German <Paul.German@us.sogeti.com>
2018-01-10 11:10:30 -0600

    main code

---
+++ test.js
@@ -0,0 +1,15 @@
+/*Begin Setup*/
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
+
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+/*End Setup*/
+
+driver.get('http://google.com');
+driver.quit();
